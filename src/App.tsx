a2b07790178da6d70b686e1540873b9c6d6d4ec7
dadumeer469-eng <dadumeer469@gmail.com>
2026-06-24 13:54:36 -0700

    feat: add PWA installation prompt component
    Implement InstallPrompt to guide users through PWA installation via a floating banner and installation modal, improving cross-platform accessibility and mobile engagement.

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import GroceryCartDrawer from "./components/GroceryCartDrawer";
 import OrderSuccessAnimation from "./components/OrderSuccessAnimation";
 import OrderHistoryDrawer from "./components/OrderHistoryDrawer";
 import daduLogo from "./assets/images/dadu_food_logo_new_1782333467889.jpg";
+import InstallPrompt from "./components/InstallPrompt";
 
 // Icons & Motion
 import { 
@@ -1632,6 +1633,9 @@ export default function App() {
           </a>
         </p>
       </footer>
+
+      {/* PWA Guided & Automatic Installation Dialog & Floating Banner */}
+      <InstallPrompt />
 
     </div>
   );
